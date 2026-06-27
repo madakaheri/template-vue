ef3487d2948c8c8b8c6149c2e0185116d417b1f9
MadakaHeri <madakaheri@icloud.com>
2026-06-27 18:01:32 +0900

    fix

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,24 @@
 <script setup></script>
 
 <template>
-  <v-app>
-    <v-app-bar>
-      <v-app-bar-nav-icon></v-app-bar-nav-icon>
-      <router-link to="/" class="text-h6 text-decoration-none">
-        <v-app-bar-title>Template Vue</v-app-bar-title>
-      </router-link>
-      <v-spacer></v-spacer>
-      <v-btn
-        to="/about"
-        variant="text"
-        text="About"
-        ></v-btn
-      >
-    </v-app-bar>
-    <v-main>
-      <router-view />
-    </v-main>
-  </v-app>
+	<v-app>
+		<v-app-bar>
+			<v-app-bar-nav-icon></v-app-bar-nav-icon>
+			<router-link
+				to="/"
+				class="text-h6 text-decoration-none"
+			>
+				<v-app-bar-title>Template Vue</v-app-bar-title>
+			</router-link>
+			<v-spacer></v-spacer>
+			<v-btn
+				to="/about"
+				variant="text"
+				text="About"
+			></v-btn>
+		</v-app-bar>
+		<v-main>
+			<router-view />
+		</v-main>
+	</v-app>
 </template>
